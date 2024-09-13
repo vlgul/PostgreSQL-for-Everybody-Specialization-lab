b1e898f3c0a437e78631d430208c815a94e6b588
Mateusz Stępień <mstepien7@protonmail.com>
2024-09-13 14:04:37 +0200

    lab1

---
+++ course 2/lab1.sql
@@ -0,0 +1,20 @@
+CREATE TABLE pg4e_debug (
+  id SERIAL,
+  query VARCHAR(4096),
+  result VARCHAR(4096),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(id)
+);
+
+SELECT query, result, created_at FROM pg4e_debug;
+
+CREATE TABLE pg4e_result (
+  id SERIAL,
+  link_id INTEGER UNIQUE,
+  score FLOAT,
+  title VARCHAR(4096),
+  note VARCHAR(4096),
+  debug_log VARCHAR(8192),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP
+);
