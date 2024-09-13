92707afc32973599fc12809ec41fdbbd57477066
Mateusz Stępień <mstepien7@protonmail.com>
2024-09-13 14:32:36 +0200

    lab 1

--- course 2/lab1.sql
+++ course 2/lab1.sql
@@ -18,3 +18,31 @@ CREATE TABLE pg4e_result (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP
 );
+
+alter table pg4e_debug add neon263 INTEGER;
+
+SELECT neon263 FROM pg4e_debug LIMIT 1;
+
+
+SELECT distinct state FROM taxdata order by state LIMIT 5;
+
+CREATE TABLE keyvalue ( 
+  id SERIAL,
+  key VARCHAR(128) UNIQUE,
+  value VARCHAR(128) UNIQUE,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  PRIMARY KEY(id)
+);
+
+create or replace function trigger_set_timestamp()
+returns trigger as $$
+begin
+	NEW.updated_at = NOW();
+	return NEW;
+end; $$ language plpgsql;
+
+create trigger set_timestamp
+before update on keyvalue
+for each row 
+execute procedure trigger_set_timestamp();
